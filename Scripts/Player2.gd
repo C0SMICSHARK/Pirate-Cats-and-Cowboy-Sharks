c8ec8ea8123769e78753eea6e21e3c2e4176358d
TattierPlutoMMU <24826524@stu.mmu.ac.uk>
2025-10-29 18:44:35 +0000

    Bullet attempt1

--- Scripts/Player2.gd
+++ Scripts/Player2.gd
@@ -1,21 +1,33 @@
 extends CharacterBody2D
-
+var bullet_path=preload("res://Scenes/Bullet.tscn")
 
 const SPEED = 400.0
 #const JUMP_VELOCITY = -400.0
+	
 
 func _ready() -> void:
 	add_to_group("can_interact_with_water")
 
+func fire():
+	var Bullet=bullet_path.instantiate()
+	Bullet.dir=rotation
+	Bullet.pos=$Node2D.global_position
+	Bullet.rota=global_rotation
+	get_parent().add_child(Bullet)
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	look_at(get_global_mouse_position())
+	if Input.is_action_just_pressed("BREAK"):
+		fire()
 
 	# Handle jump.
 	#if Input.is_action_just_pressed("JUMP_P2") and is_on_floor():
 	#	velocity.y = JUMP_VELOCITY
-
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("LEFT_P2", "RIGHT_P2")
