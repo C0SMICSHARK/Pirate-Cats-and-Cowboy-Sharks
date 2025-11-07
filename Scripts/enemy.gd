f6f2adb75f940f9a74b748ce2d7b5f59b0af1791
TattierPlutoMMU <24826524@stu.mmu.ac.uk>
2025-11-07 15:22:27 +0000

    Improved bullet collision and anim fix

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -3,4 +3,9 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$"../MovingPlatform/AnimationPlayer".play("EnemySideToSide")
+	add_to_group("Enemy")#Useful for collisons
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
