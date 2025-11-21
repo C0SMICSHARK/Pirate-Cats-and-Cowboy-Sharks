9c21a5817fbedb8c444d97199c65d84feaf85924
C0SMICSHARK <Xanderrowley42@gmail.com>
2025-11-21 11:33:21 +0000

    Deleted Corrupted Scene

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -20,9 +20,10 @@ func _on_quit_pressed() -> void:
 
 
 func _on_fade_timer_timeout() -> void:
-	if button_type == "start" :
-		get_tree().change_scene_to_file("res://Scenes/Levels/TestScene_1.tscn")
+	pass
+	#if button_type == "start" :
+		#get_tree().change_scene_to_file("res://Scenes/Levels/TestScene_1.tscn")
 	
-	elif button_type == "options" :
-		get_tree().change_scene_to_file("res://Scenes/Levels/TestScene_1.tscn")
+	#elif button_type == "options" :
+		#get_tree().change_scene_to_file("res://Scenes/Levels/TestScene_1.tscn")
 	#Idealy this would change to an options screen rather than the level
