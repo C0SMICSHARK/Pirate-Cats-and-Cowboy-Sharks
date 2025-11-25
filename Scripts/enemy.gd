3b8c634af01e9497b6920cecef4b222e942778ff
TattierPlutoMMU <24826524@stu.mmu.ac.uk>
2025-11-25 23:24:31 +0000

    Made setting up enemy movement as simple as possible

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -5,6 +5,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	add_to_group("Enemy")#Useful for collisons
+	AnimPlayer.play("EnemySideToSide")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
