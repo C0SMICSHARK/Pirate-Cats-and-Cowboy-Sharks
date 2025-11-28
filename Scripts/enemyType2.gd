49712c43ed33f9c020db5f535e6fb02f5bc6bf6b
TattierPlutoMMU <24826524@stu.mmu.ac.uk>
2025-11-28 19:27:01 +0000

    Spear dino walk animations working

---
+++ Scripts/enemyType2.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+@export var EnemyHealth = 2 * Global.Difficulty
+@export var AnimPlayer = Node
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	add_to_group("Enemy")#Useful for collisons
+	AnimPlayer.play("EnemySideToSide")
+	$CharacterBody2D/AnimatedSprite2D.play("WalkLeft")
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta) -> void:
+	modulate = Color(1,clamp(modulate.g + delta,0,1),clamp(modulate.b + delta,0,1))
+	if $StunTimer.is_stopped():
+		AnimPlayer.play()
+	if EnemyHealth <= 0:
+		queue_free()
+	if AnimPlayer.get_current_animation_position() <= (AnimPlayer.get_current_animation_length() / 2):
+		$CharacterBody2D/AnimatedSprite2D.play("WalkRight")
+	else :
+		$CharacterBody2D/AnimatedSprite2D.play("WalkLeft")
+
+
+func _on_hurtbox_body_entered(body: Node2D) -> void:
+	if body.is_in_group(	"Sword"):
+		AudioController.play_SwordHit()
+	
+	if body.is_in_group(	"PlayerAttack"):
+		EnemyHealth -= 1
+		modulate = Color(1,0,0)
+		if body.is_in_group("Projectile"):
+			body.queue_free()
+		AnimPlayer.pause()
+		$StunTimer.start()
+		
+	
+	
