47f6255a34cfcdd0009e87d02b6e2e4111d1aac3
C0SMICSHARK <Xanderrowley42@gmail.com>
2025-12-06 20:41:30 +0000

    4th Level Assets Implemented

--- Scripts/level_end.gd
+++ Scripts/level_end.gd
@@ -9,7 +9,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	if PlayersNeeded > 2:
-		get_tree().change_scene_to_file("res://Assets/Misc/End.tscn")
+		get_tree().change_scene_to_file("res://Scenes/Levels/level_4.tscn")
 
 
 func _on_body_shape_entered(_body_rid: RID, _body: Node2D, _body_shape_index: int, _local_shape_index: int) -> void:
