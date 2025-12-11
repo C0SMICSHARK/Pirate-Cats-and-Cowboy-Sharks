327e5a577db648ef3b004c6bc55eeee9f311a5c4
TattierPlutoMMU <24826524@stu.mmu.ac.uk>
2025-12-11 05:28:59 +0000

    Added temporary fireball test, and more art

---
+++ Scripts/fire_ball.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if $ImpactTimer.is_stopped():
+		$RigidBody2D/MainBall.play("Ball")
+		$RigidBody2D.freeze = false
+		
+	if not $ImpactTimer.is_stopped():
+		$RigidBody2D.freeze = true
+
+func _on_rigid_body_2d_body_entered(_body: Node) -> void:
+	$RigidBody2D/CollisionShape2D.position.y = -40000
+	$RigidBody2D/MainBall.play("Hidden")
+	$ImpactTimer.start()
+	$RigidBody2D/Impact.play("Impact")
+
+func _on_impact_animation_finished() -> void:
+	#$RigidBody2D.position.y = 0
+	#$RigidBody2D.constant_force.y = 0
+	queue_free()
